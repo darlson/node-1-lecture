0e79d8dd14e55faae3849bada231803628582b73
darlson <davertiser@gmail.com>
2020-05-19 11:52:15 -0600

    completed lecture node practice

---
+++ nodeTest.js
@@ -0,0 +1,17 @@
+const david = {
+    name: 'David',
+    age: 32,
+    favorites: {
+        color: 'grey',
+        number: 8,
+        person: 'Becca'
+    }
+}
+
+function sayMyName (name){
+    console.log(`Hello, my name is ${name}`)
+}
+
+sayMyName(david.name)
+console.log('I pity da foo')
+
---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const ctrl = require('./controller')
+const SERVER_PORT = 4444
+
+app.use(express.json())
+
+app.use(function (req, res, next) {
+    console.log("We got a request, cap'n")
+    next()
+})
+
+app.get('/api/andrew', (req, res) =>{
+    console.log('I GOT A REQUEST AT /api/andrew')
+    res.status(200).send('Andrew')
+})
+
+app.get('/api/users', ctrl.getAllUsers)
+app.get('/api/users/:user_id', ctrl.getUserById)
+app.post('/api/users', ctrl.createUser)
+app.put('/api/users/:user_id', ctrl.editUser)
+app.delete('/api/users/:user_id', ctrl.deleteUser)
+
+
+
+
+
+
+
+
+
+
+app.listen(SERVER_PORT, () =>
+    console.log(`Server running on port ${SERVER_PORT}`))
